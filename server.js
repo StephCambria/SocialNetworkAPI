9a86d0d75c8099c881651a3a568bf08647901199
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-10 15:17:20 -0500

    just keeping things up to date

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const db = require("./config/connection"); // mongoose will be stored here
 // Require routes
-const routes = require('./routes');
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
